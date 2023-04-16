33935789c996e6734e53c7816127ec3aa0d08b2c
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2023-04-16 19:14:47 +0200

    feat(project): editorconfig, prettier and tsconfig base provided

--- src/core.ts
+++ src/core.ts
@@ -1,24 +1,7 @@
-import { Observable } from "rxjs";
-
 export class Core {
-  id: number;
-  projectName: String;
-  constructor() {
-    const smth = new Observable((subscriber) => {
-      subscriber.next("FELEK");
-      subscriber.next("STASIA");
-      setTimeout(() => {
-        subscriber.next("TOMEK");
-        subscriber.complete();
-      }, 2000);
-    });
-    smth.subscribe({
-      next(x) {
-        console.log(`twoje imie to ${x}`);
-      },
-      error(err) {},
-      complete() {},
-    });
-    console.log("PESTO 🐶");
-  }
+    constructor(public id: number, public projectName: String) {}
+}
+
+class Test {
+    constructor() {}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { Core } from "./core";
 
-const myFamily = new Core();
+const myFamily = new Core(1, "Family");

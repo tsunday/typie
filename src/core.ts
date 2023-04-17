f5a93bacd8ec7766dd8f6002ce8e548f97fb926d
Tomasz Niedziela-Brach <niedziela.brach@gmail.com>
2023-04-17 22:24:40 +0200

    feat(core): new amazing functionality

--- src/core.ts
+++ src/core.ts
@@ -1,7 +1,11 @@
 export class Core {
-    constructor(public id: number, public projectName: String) {}
+  constructor(public id: number, public projectName: String) {}
+
+  updateProjectName(newProjectName: String) {
+    this.projectName = newProjectName;
+  }
 }
 
 class Test {
-    constructor() {}
+  constructor() {}
 }
